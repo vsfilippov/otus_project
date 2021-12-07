cd0b14cfa9ea5f3f132fa672b0dcf6cc0224974a
v.s.filippov <v.s.filippov@propellerads.net>
2021-12-07 21:37:44 +0300

    setting environment

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # otus_project
-otus_homework_project
+otus_project environment
+
+to start:
+git clone
+docker-compose up -d
+
+to enter the container:
+docker exec -it otus_project bash
+
+http://localhost:8000
---
+++ src/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'root/html',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'root/html' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
